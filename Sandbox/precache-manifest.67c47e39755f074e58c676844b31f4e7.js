e44ec4b1d5c5a75a378e153de4f2bc59136c0db6
Ving Trung <vingtrung@gmail.com>
2019-08-09 23:16:43 -0700

    fix index and add stuff

---
+++ Sandbox/precache-manifest.67c47e39755f074e58c676844b31f4e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919828fadec34acc1a676a96a6805420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4dcfc6377d99d1114a",
+    "url": "/static/css/main.27f19759.chunk.css"
+  },
+  {
+    "revision": "80c9294de0f8a3fbd9a2",
+    "url": "/static/js/2.d5a8a78f.chunk.js"
+  },
+  {
+    "revision": "3d4dcfc6377d99d1114a",
+    "url": "/static/js/main.f3f27d40.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
